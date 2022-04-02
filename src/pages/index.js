126ae45f711bf4c9de4d4ff2e9140de92a9012af
shahriar <shahriar.mehedi@bjitgroup.com>
2022-04-03 02:58:31 +0600

    ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import {useRouter} from "next/router"
 
 export default function Home({tasks = []}) {
+  const router=useRouter();
   if(tasks.length===0){
     return (
         <Grid centered verticalAlign="middle" columns="1" style={{height:"80vh"}}>
@@ -11,7 +12,7 @@ export default function Home({tasks = []}) {
               <h1>There are no task present</h1>
               <div>
                 <Button primary onClick={()=> router.push("/tasks/CreateTask")}>
-
+                    Create Task
                 </Button>
               </div>
             </Grid.Column>
@@ -19,5 +20,42 @@ export default function Home({tasks = []}) {
         </Grid>
     )
   }
+  return(
+    <Container>
+      <Card.Group itemsPerRow={4}>
+        {tasks && 
+        tasks.map((task)=>(
+          <Card key={task._id}>
+              <Card.Content>
+                <Card.Header>
+                  <Link href={`/tasks/${task._id}`}>
+                    <a>{task.title}</a>
+                  </Link>
+                </Card.Header>
+                <p>{task.description}</p>
+              </Card.Content>
+              <Card.Content extra>
+                <Button  color="orange" onClick={()=>router.push(`/tasks/${task._id}`)}>
+                  View
+                </Button>
+                <Button  color="blue" onClick={()=>router.push(`/tasks/${task._id}/edit`)}>
+                  Edit
+                </Button>
+              </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </Container>
+  )
 
+}
+export async function getServerSideProps(){
+  const response = await fetch("http://localhost:3000/api/tasks");
+  const tasks = await response.json();
+
+  return{
+    props:{
+      tasks,
+    }
+  }
 }
