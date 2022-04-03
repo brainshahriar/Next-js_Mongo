6a3fbf8fb3fcbb8e60009e0199108193e7117f80
Al Shahriar Mehedi <89078425+brainshahriar@users.noreply.github.com>
2022-04-03 19:58:38 +0600

    Update Task.js

--- src/model/Task.js
+++ src/model/Task.js
@@ -9,6 +9,7 @@ const TaskSchema = new Schema(
             trim:true,
             maxlength:[40,"title cannot be greater than 40 characters"],
         },
+        
         description:{
             type:String,
             required:[true,"The task description is required"],
